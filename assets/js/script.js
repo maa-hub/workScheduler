57cc60319cc6234eacdca7ea11ddb87a99924d1b
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-05-22 20:36:57 -0400

    local storage added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,53 @@
 
-var today = moment().format('dddd, MMM Do YYYY');
+var today = moment().format('dddd, MMM Do YYYY h:mm:ss a');
 $("#currentDay").text(today);
 
-var tasks = {};
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+    
+        var task = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, task);
+    })
 
 
+function currentTime() {
+   var timeHour= moment().hour();
+   
+$(".time-block").each(function () {
+  var taskTime = parseInt($(this).attr("id"));  
 
+    if (taskTime < timeHour) {
+    $(this).removeClass("present")
+    $(this).removeClass("future")
+    $(this).addClass("past");
+    }
+    else if (taskTime === timeHour) {
+    $(this).removeClass("present")
+    $(this).removeClass("future")
+    $(this).addClass("present");
+    }
+    else if (taskTime > timeHour){
+    $(this).removeClass("present")
+    $(this).removeClass("future")
+    $(this).addClass("future");
+    }
+})
+}
 
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+currentTime();
+
+})
